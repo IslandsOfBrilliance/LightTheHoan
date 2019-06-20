6f77014111c76032d32636e311c4d13d19c4104a
Purpzie <25022704+Purpzie@users.noreply.github.com>
2019-06-20 14:48:10 -0500

    Add control panel placeholder

---
+++ Assets/Scripts/Movement/Controller.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] XRNode device;
+}
