287a7f2dea3354972ad9ad6779f24edccbc84a48
Mojang <47279783+AdminIsOffline@users.noreply.github.com>
2019-06-20 14:44:51 -0500

    Added Controlers+hands

---
+++ Assets/Controller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Controller : MonoBehaviour{
+    [SerializeField]
+    XRNode device;
+
+}
