680b243e77e514b0104ad7f0134c52205721f980
Mojang <47279783+AdminIsOffline@users.noreply.github.com>
2019-06-24 15:53:42 -0500

    added audio, added summerfest building, added lighthouse

---
+++ Assets/Scripts/Audio/DelayedLoop.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DelayedLoop : MonoBehaviour
+{
+    public AudioClip audioClip;
+    [Range(0f, 30f)]
+    public float minDelay;
+    [Range(0f, 30f)]
+    public float maxDelay;
+
+    AudioSource source;
+    bool playingAudio;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = audioClip;
+    }
+
+    IEnumerator PlayAudio()
+    {
+        playingAudio = true;
+        float delay = Random.Range(minDelay, maxDelay);
+        yield return new WaitForSeconds(delay);
+        source.Play();
+        playingAudio = false;
+    }
+
+    private void Update()
+    {
+        if (!source.isPlaying && !playingAudio)
+            StartCoroutine(PlayAudio());
+    }
+}
