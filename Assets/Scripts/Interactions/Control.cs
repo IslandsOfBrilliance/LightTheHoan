f46c5df786b15347a1e4875847de97d5745ddda7
Purpzie <25022704+Purpzie@users.noreply.github.com>
2019-06-24 13:48:28 -0500

    finish grab/throw

--- Assets/Scripts/Interactions/Control.cs
+++ Assets/Scripts/Interactions/Control.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
-    [SerializeField] OVRInput.Controller controller;
+    public OVRInput.Controller controller;
+    [SerializeField] float collisionSize = .1f;
+    [SerializeField] LayerMask interactable;
+    public Collider[] touching;
+
     void Update()
     {
         transform.localPosition = OVRInput.GetLocalControllerPosition(controller);
         transform.localRotation = OVRInput.GetLocalControllerRotation(controller);
+        touching = Physics.OverlapSphere(transform.position, collisionSize, interactable);
     }
 }
