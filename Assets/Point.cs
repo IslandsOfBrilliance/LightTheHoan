b46f1654677288e247438560d7ff6952b53dcdb2
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-06-24 15:57:45 -0500

    fixed some issues after merge

--- Assets/Point.cs
+++ Assets/Point.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Point : MonoBehaviour{
+public class Point : MonoBehaviour
+{
     private Control pointControl;
-    private void Start(){ /*private void Start "RUNS ONCE AT RUNTIME_STARTUP"*/
+
+    private void Start()
+    { /*private void Start "RUNS ONCE AT RUNTIME_STARTUP"*/
         pointControl = GetComponent<Control>();
     }///<summary>///CODE_USED_TO_DETECT_POINTABLE_OBJECTS_AND_POINT///</summary>
-    private void Update(){ /*private void update "UPDATES EVERY FRAME"*/
+
+    private void Update()
+    { /*private void update "UPDATES EVERY FRAME"*/
         OVRInput.Update();
-        if (pointControl.interactables.Length > 0 && pointControl.interactables[0].tag == "Pointable" && pointControl.controller == OVRInput.Controller.LTouch ? OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) : OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger)){
-            print("print{PRINT_VOID([SERIAL_MONITOR]:POINTABLE_FOUND)}"); //Pointable Object {boolean "FOUND"(1)} Through Raycasting
-            Interaction interaction = pointControl.interactables[0].GetComponent<Interaction>();
-            if (interaction != null){
-                interaction.Interact();
+        if (pointControl.touching.Length > 0 && pointControl.touching[0].tag == "Pointable" && pointControl.controller == OVRInput.Controller.LTouch ? OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) : OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger)){
+
+            if(pointControl.touching.Length > 0 && pointControl.touching[0])
+            {
+                Interaction interaction = pointControl.touching[0].GetComponent<Interaction>();
+                if (interaction != null)
+                {
+                    interaction.Interact();
+                }
             }
         }
         else{
