b56293f8e1732721fefc43adf20af9514304029d
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-06-25 13:34:30 -0500

    added infinite spawn bool

--- Assets/SpawnBulb.cs
+++ Assets/SpawnBulb.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class SpawnBulb : MonoBehaviour{
+public class SpawnBulb : MonoBehaviour
+{
     private GameObject bulb;
     public Transform spawnPosition;
     public GameObject lightbulb;
-    public void BulbSpawn(){
-        if(bulb == null){
+
+    private bool infiniteSpawn;
+
+    public void BulbSpawn()
+    {
+        if(bulb == null || infiniteSpawn)
+        {
             bulb = Instantiate(lightbulb, spawnPosition.position, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F4))
+        {
+            infiniteSpawn = !infiniteSpawn;
         }
     }
 }
