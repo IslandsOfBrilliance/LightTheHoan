a0991d4de4234c1401a39ccf30af56410124554a
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-06-25 12:01:50 -0500

    Spawn ONCE

--- Assets/Scripts/Interactables/ButtonInteraction.cs
+++ Assets/Scripts/Interactables/ButtonInteraction.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class ButtonInteraction : Interaction{
+public class ButtonInteraction : Interaction
+{
+    public SpawnBulb bulby;
     public Animator animator;
     private bool isPressed;
-    public override void Interact(){
+    public override void Interact()
+    {
         if (isPressed == false)
         {
             isPressed = true;
             animator.SetTrigger("ButtonTrig");
+            bulby.BulbSpawn();
             StartCoroutine(Pressed());
         }
     }
-    IEnumerator Pressed(){
+    IEnumerator Pressed()
+    {
         yield return new WaitForSeconds(1);
         isPressed = false;
     }
---
+++ Assets/SpawnBulb.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SpawnBulb : MonoBehaviour{
+    private GameObject bulb;
+    public Transform spawnPosition;
+    public GameObject lightbulb;
+    public void BulbSpawn(){
+        if(bulb == null){
+            bulb = Instantiate(lightbulb, spawnPosition.position, Quaternion.identity);
+        }
+    }
+}
